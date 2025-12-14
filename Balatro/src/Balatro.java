e6b21126bc81803809476fe500113b30698229b2
ajwar <ajwar@Ender_Chest>
2025-12-13 18:51:24 -0700

    -at home commit of school project

--- Balatro/src/Balatro.java
+++ Balatro/src/Balatro.java
@@ -5,7 +5,7 @@ public class Balatro
 
 		public static void main(String[] args)
 			{
-			System.out.println("YOOOOOOOOOOOO finally It is a repo... Turns out I had a ton of updates that needed to be done, so Its all set now!");
+			System.out.println("YOOOOOOOOOOOO finally It is a repo... Turns out I had a ton of updates that needed to be done, so Its all set now! =)");
 			//	Deck.fillDeck();
 			//	Deck.fillModifiedDeck();
 			//	Deck.shuffleDeck();
