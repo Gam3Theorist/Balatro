c0b55f8e0646b54472ea527a85a95b9dc246fdf1
Adam Ward <adam.ward@mullen.pvt.k12.co.us>
2026-02-04 10:55:35 -0700

    balls

--- Balatro/src/Balatro.java
+++ Balatro/src/Balatro.java
@@ -5,10 +5,10 @@ public class Balatro
 
 		public static void main(String[] args)
 			{
-			System.out.println("YOOOOOOOOOOOO finally It is a repo... Turns out I had a ton of updates that needed to be done, so Its all set now! hahaha please commit well");
-			//	Deck.fillDeck();
-			//	Deck.fillModifiedDeck();
-			//	Deck.shuffleDeck();
+			
+				Deck.fillDeck();
+				Deck.fillModifiedDeck();
+				Deck.shuffleDeck();
 				
 				
 
